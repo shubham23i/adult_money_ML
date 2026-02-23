0a67561f1e8a5c1960bc273e6c1d1220f1465451
Shubham Joshi <indsjos@gmail.com>
2026-02-23 10:22:34 +0530

    second commit

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages,setup
+from typing import List
+
+hyphen_dot_e = '-.e'
+def get_requirements(file_path:str)->List[str]:
+    requirements=[]
+
+
+    with open(file_path)as file_obj:
+        requirements=file_obj.readline()
+        requirements=[req.replace('/n','') for req in requirements]
+        if hyphen_dot_e in requirements:
+            requirements.remove(hyphen_dot_e)
+    return requirements
+        
+
+
+
+
+
+
+
+setup(
+    name='adult_money_ML',
+    version='0.0.1',
+    author='Shubham Joshi',
+    author_email='indsjos@gmail.com',
+    packages=find_packages(),
+    install_requires=['numpy','pandas']
+)
